04f91b6572bafa15699cfcf3dc85d10670064388
Rubber <wh85424@gmail.com>
2023-06-01 18:02:50 +0900

    Step05 File 업로드 기능 추가

--- Step05Final/src/main/java/test/filter/LoginFilter.java
+++ Step05Final/src/main/java/test/filter/LoginFilter.java
@@ -13,8 +13,8 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-@WebFilter("/private/*")
+// urlPatterns = String[] 
+@WebFilter(urlPatterns = {"/private/*", "/file/private/*"})
 public class LoginFilter implements Filter{
 
 	@Override
