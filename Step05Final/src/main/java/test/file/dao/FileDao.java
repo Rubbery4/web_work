8e0ebff512da362d08d845e62b0c4c518fc75565
Rubber <wh85424@gmail.com>
2023-06-09 12:59:34 +0900

    리스트 목록 구현2

--- Step05Final/src/main/java/test/file/dao/FileDao.java
+++ Step05Final/src/main/java/test/file/dao/FileDao.java
@@ -156,8 +156,60 @@ public class FileDao {
 		}
 		return list;
 	}
+	// 페이지에 해당하는 목록만 리턴하는 메소드
+	public List<FileDto> getList(FileDto dto) {
+		List<FileDto> list = new ArrayList<>();
+		// 필요한 객체의 참조값을 담을 지역변수 미리 만들기
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			//DbcpBean 객체를 이용해서 Connection 객체를 얻어온다 (Connection Pool 에서 얻어오기)
+			conn = new DBcpBean().getConn();
+			// 실행할 sql 문 (select)
+			String sql = "SELECT *"
+					+ " FROM"
+					+ "    (SELECT result1.*, ROWNUM AS rnum"
+					+ "     FROM "
+					+ "       (SELECT num, writer, title, orgFileName, saveFileName, fileSize, regdate"
+					+ "        FROM board_file"
+					+ "        ORDER BY num DESC) result1)"
+					+ " WHERE rnum BETWEEN ? AND ?";
+			pstmt = conn.prepareStatement(sql);
+			// sql 문이 미완성이라면 여기서 완성
+			pstmt.setInt(1, dto.getStartRowNum());
+			pstmt.setInt(2, dto.getEndRowNum());
+			// select 문 수행하고 결과값 받아오기
+			rs = pstmt.executeQuery();
+			// 반복문 돌면서 ResultSet 에 담긴 애용 추출
+			while(rs.next()) {
+				// ResultSet 에 cursor 가 위치 한곳의 칼럼 정보를 얻어와서 MemberDto 객체에 담고
+				dto = new FileDto();
+				dto.setNum(rs.getInt("num"));
+				dto.setWriter(rs.getString("writer"));
+				dto.setTitle(rs.getString("title"));
+				dto.setOrgFileName(rs.getString("orgFileName"));
+				dto.setSaveFileName(rs.getString("saveFileName"));
+				dto.setFileSize(rs.getLong("fileSize"));
+				dto.setRegdate(rs.getString("regdate"));
+				// ArrayList 객체에 누적 시키기
+				list.add(dto);				
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs!=null)rs.close();
+				if(pstmt!=null)pstmt.close();
+				if(conn!=null) conn.close();
+			} catch(Exception e) {}
+		}
+		// 회원 목록이 담긴 ArrayList 객체 리턴해주기
+		return list;	
+	}
 	
-	public List<FileDto> getList() {
+	
+	public List<FileDto> getListAll() {
 		List<FileDto> list = new ArrayList<>();
 		// 필요한 객체의 참조값을 담을 지역변수 미리 만들기
 		Connection conn = null;
--- Step05Final/src/main/webapp/file/list.jsp
+++ Step05Final/src/main/webapp/file/list.jsp
@@ -4,10 +4,32 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
 <%
-FileDao dao = FileDao.getInstance();
-List<FileDto> list = dao.getList();
-// 로그인된 아이디( 로그인이 되어있지 않으면 null)
-String id = (String)session.getAttribute("id");
+	// 한페이지에 몇개씩 표시할 것인지
+	final int PAGE_ROW_COUNT=5;
+
+	// 보여줄 페이지의 번호를 일단 1이라고 초기값 지정
+	int pageNum=1;
+	
+	// 페이지 번호가 파라미터로 전달되는지 읽어와 보기
+	String strPageNum= request.getParameter("pageNum");
+	// 만일 페이지 번호가 파라미터로 넘어 온다면
+	if(strPageNum != null) {
+		// 숫자로 바꿔서 보여줄 페이지 번호를 지정한다.
+		pageNum=Integer.parseInt(strPageNum);
+	}
+	// 보여줄 페이지의 시작 ROWNUM
+	int startRowNum = 1+(pageNum-1)*PAGE_ROW_COUNT;
+	// 보여줄 페이지의 끝 ROWNUM
+	int endRowNum = pageNum*PAGE_ROW_COUNT;
+	
+	FileDto dto = new FileDto();
+	dto.setStartRowNum(startRowNum);
+	dto.setEndRowNum(endRowNum);
+	
+	FileDao dao = FileDao.getInstance();
+	List<FileDto> list = dao.getList(dto);
+	// 로그인된 아이디( 로그인이 되어있지 않으면 null)
+	String id = (String)session.getAttribute("id");
 %>
 <!DOCTYPE html>
 <html>
